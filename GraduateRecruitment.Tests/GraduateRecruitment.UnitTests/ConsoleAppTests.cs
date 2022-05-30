fc59262e132bb1325a68008642aded89552171dc
Charles Loxton <charlesloxtonf@gmail.com>
2022-05-30 17:25:17 +0200

    Completed questions and Unit tests

--- GraduateRecruitment.Tests/GraduateRecruitment.UnitTests/ConsoleAppTests.cs
+++ GraduateRecruitment.Tests/GraduateRecruitment.UnitTests/ConsoleAppTests.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
-using FluentAssertions.Execution;
+using GraduateRecruitment.ConsoleApp;
+using GraduateRecruitment.ConsoleApp.Data;
 using NUnit.Framework;
 
 namespace GraduateRecruitment.UnitTests
@@ -9,5 +10,54 @@ namespace GraduateRecruitment.UnitTests
     [TestFixture]
     public class ConsoleAppTests
     {
+        [Test]
+        public void GetHighestAverage_When_Wednesday_Then_ReturnsSavannaDry()
+        {
+            var repo = new OpenBarRepository();
+
+            var result = Program.GetHighestAverage("Wednesday", repo);
+
+            result.Keys.First().Should().Be("Savanna Dry");
+            result.Values.First().Should().Be(6);
+        }
+
+        [Test]
+        public void DrinkQuantity()
+        {
+            var repo = new OpenBarRepository();
+
+            var result = Program.DrinkQuantity("Fanta Orange", new DateTime(2022, 04, 20), repo);
+
+            result.Should().Be(3);
+        }
+
+        [Test]
+        public void GetDrinkPrice()
+        {
+            var repo = new OpenBarRepository();
+
+            var result = Program.GetDrinkPrice("Ceres Orange Juice", repo);
+
+            result.Should().Be(8.99M);
+        }
+
+        [Test]
+        public void latestRecord()
+        {
+            var repo = new OpenBarRepository();
+
+            var result = Program.latestRecord(repo);
+
+            result.Should().Be(repo.AllOpenBarRecords[repo.AllOpenBarRecords.Count - 1].Date);
+        }
+        [Test]
+        public void CalculatePopularity()
+        {
+            var repo = new OpenBarRepository();
+
+            var result = Program.CalculatePopularity("Savanna Dry", repo);
+
+            result.Should().Be(13);
+        }
     }
 }
